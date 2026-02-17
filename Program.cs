66376d5c096fce31782195d7281968cf6b6e91d9
rosvend <roy.sandoval@upb.edu.co>
2026-02-17 08:52:36 -0500

    feat: add presentation layer and wire up composition root
    Add Presentation/ConsoleUI.cs that handles all Console I/O, depending
    only on abstractions via constructor injection (DIP). All menu logic,
    input validation loops, and user-facing text live here exclusively (SRP).
    
    Rewrite Program.cs as a clean composition root that wires:
    - InMemoryUserRepository + InMemoryProductRepository (repositories)
    - AuthenticationService (depends on IUserRepository)
    - ShoppingCart (implements ICart)
    - PaymentService with CardPayment/CashPayment/AccountPayment (OCP)
    - InvoiceService (depends on ICart)
    - NameValidator/EmailValidator/PhoneValidator (ISP)
    
    Remove all legacy classes (Usuario, Registro, Cliente, Producto,
    Carrito, Pagar, Factura, Ofertas) that violated SOLID principles
    with static state, mixed concerns, and tight coupling.
    
    The application preserves the original user-facing flow (login/register,
    browse products, add/remove from cart, pay, generate invoice) while
    being fully SOLID-compliant and testable.

--- Program.cs
+++ Program.cs
@@ -1,342 +1,64 @@
-﻿using System;
-using TiendaOnline;
+using TiendaOnline.Abstractions;
+using TiendaOnline.Domain;
+using TiendaOnline.Presentation;
+using TiendaOnline.Services;
 
-namespace TiendaOnline
+// --- Composition Root: wire all dependencies ---
+
+// Repositories
+var userRepo = new InMemoryUserRepository();
+var productRepo = new InMemoryProductRepository();
+
+// Seed a default user so login works out of the box
+userRepo.Register(new RegistrationData
 {
-
-    class Program
-    {
-        static void Main()
-        {
-
-
-            // Creacion de algunos productos
-            Producto.AgregarProducto(1, "Portatil", 1000, 1);
-            Producto.AgregarProducto(2, "Celular", 550, 1);
-            Producto.AgregarProducto(3, "Airpods", 80, 1);
-            Producto.AgregarProducto(4, "tarjeta grafica", 800, 1);
-            Producto.AgregarProducto(5, "Mouse", 22.9, 1);
-            Producto.AgregarProducto(6, "Parlantes", 300, 1);
-            Producto.AgregarProducto(7, "Bose", 400, 1);
-            
-            int opcion, opcion1, opcion2, opcion3,opcion4,opcion5,opcion6;
-
-
-                
-            {
-                Console.WriteLine("-----BIENVENIDO-----\n");
-                Console.WriteLine("1.Iniciar Sesion");
-                Console.WriteLine("2.Registrarse \n");
-                Console.Write("Seleccione una opción: ");
-
-                if (int.TryParse(Console.ReadLine(), out opcion))
-                {
-                    switch (opcion)
-                    {
-                        case 1:
-                            Console.WriteLine();
-                            Usuario.IngresarUsuario();
-                            Console.WriteLine("\nINICIO EXITOSO\n");
-                            break;
-                        case 2:
-                            Console.WriteLine();
-                            Registro.RegistrarUsuario();
-                            Console.WriteLine("\nREGISTRO EXITOSO\n");
-                            break;
-                        default:
-                            Console.WriteLine("Opción no válida. Por favor, vuelva a intentar.\n");
-                            break;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Por favor ingrese solo números.\n");
-                }
-            } while (opcion != 1 && opcion != 2);
-
-
-            do
-            {
-                Console.WriteLine("-----MENU PRINCIPAL-----\n");
-
-                Console.WriteLine("1.CLIENTE");
-                Console.WriteLine("2.PRODUCTOS");
-                Console.WriteLine("3.CARRITO");
-                Console.WriteLine("0.SALIR \n");
-                Console.WriteLine("Ingrese una opcion: ");
-                opcion1 = int.Parse(Console.ReadLine());
-                Console.WriteLine();
-
-                switch (opcion1)
-                {
-
-                    case 1:
-
-                        do
-
-                        {
-
-                            Console.WriteLine("CLIENTE");
-                            Console.WriteLine("1.VerProductos");
-                            Console.WriteLine("2.VerCarrito");
-                            Console.WriteLine("0.Volver");
-                            Console.WriteLine("Elija una opcion: ");
-                            opcion2 = Convert.ToInt32(Console.ReadLine());
-                            Console.WriteLine();
-
-                            switch (opcion2)
-                            {
-
-                                case 1:
-                                    Console.WriteLine();
-                                    Console.WriteLine("LISTA DE PRODUCTOS:");
-                                    Producto.VerTodosLosProductos();
-                                    Console.WriteLine();
-
-                                    break;
-
-                                case 2:
-                                    Console.WriteLine();
-                                    Console.WriteLine("PRODUCTOS EN EL CARRITO:");
-                                    Cliente.VerProductosEnCarrito();
-                                    Console.WriteLine();
-
-                                    break;
-
-                                default:
-
-                                    Console.WriteLine("INGRESE UNA OPCION VALIDA");
-
-                                    break;
-
-                            }
-
-
-                        } while (opcion2 != 0);
-
-                        break;
-
-                    case 2:
-
-                        do
-                        {
-
-                            Console.WriteLine("PRODUCTO");
-                            Console.WriteLine("1.Agregar al carrito");
-                            Console.WriteLine("0.volver ");
-                            Console.WriteLine("Elija una opcion: ");
-                            opcion3 = Convert.ToInt32(Console.ReadLine());
-                            Console.WriteLine();
-
-
-                            switch (opcion3)
-                            {
-
-                                case 1:
-
-                                    Producto.AgregarProductoCarrito();
-
-                                    break;
-
-                                default:
-
-                                    Console.WriteLine("Ingrese un valor valido");
-
-                                    break;
-
-                            }
-
-
-                        } while (opcion3 != 0);
-
-
-                        break;
-
-                        case 3:
-
-                        do {
-
-                            Console.WriteLine("-----CARRITO-----");
-                            Console.WriteLine();
-                            Console.WriteLine("PRODUCTOS EN EL CARRITO:");
-                            Cliente.VerProductosEnCarrito();
-                            Console.WriteLine();
-                            Console.WriteLine("1.Sacar producto");
-                            Console.WriteLine("2.Calular total");
-                            Console.WriteLine("3.Hacer pago");
-                            Console.WriteLine("0.Volver \n");
-                            Console.WriteLine("Elija una opcion: ");
-                            opcion4 = Convert.ToInt32(Console.ReadLine());
-                            Console.WriteLine();
-
-                            switch (opcion4)
-                            {
-
-                                case 1:
-
-                                    Carrito.SacarProductoDelCarritoPorConsola();
-
-                                    break;
-
-                                    case 2:
-
-                                    double total = Carrito.CalcularTotal();
-                                    Console.WriteLine($"Total del Carrito: {total} \n");
-                                    Console.WriteLine();
-                                    
-
-                                    break;
-
-                                    case 3:
-
-
-                                    Console.WriteLine("-----PAGAR-----");
-
-                                    Pagar pagar = new Pagar();  
-                                    Pagar.IngresarMetodoPago();
-                                    Pagar.IngresarNumeroCuenta();
-                                    Console.WriteLine();
-                                    Console.WriteLine("ACEPTADO");
-                                    Console.WriteLine();
-
-                                    Console.WriteLine("1.Confirmar pago ");
-                                    Console.WriteLine("2.Volver \n ");
-                                    Console.WriteLine();
-                                    Console.WriteLine("Elija una opcion : ");
-                                    opcion5 = Convert.ToInt32(Console.ReadLine());  
-
-                                    switch (opcion5) 
-                                    {
-
-                                        case 1:
-
-                                            Console.WriteLine("-----FACTURA-----");
-
-                                            Factura factura = new Factura();
-                                            Console.WriteLine();
-                                            
-                                            Console.WriteLine();
-
-                                            Console.WriteLine("1.Generar recibo");
-                                            Console.WriteLine("0.volver \n");
-                                            Console.WriteLine("Elija una opcion");
-                                            opcion6 = Convert.ToInt32(Console.ReadLine());
-
-
-
-                                            switch (opcion6)
-                                            {
-
-                                                case 1:
-                                                    Console.WriteLine("\nIdentificacion: " + Factura.Identificacion() + "\nTotal a pagar: " + Carrito.CalcularTotal() + "\nFecha: " + Factura.Fecha());
-                                                    Console.WriteLine();
-                                                    Console.WriteLine();
-                                                    Console.WriteLine();
-                                                    Console.WriteLine("-----SU RECIBO A SIDO GENERADO-----");
-
-
-
-
-                                                    
-
-
-                                                    Console.WriteLine("\n\n\n\n\n");
-                                                    break;
-
-                                                default:
-
-                                                    break;
-
-                                                   
-
-
-
-                                            }
-
-
-                                            
-
-
-
-
-
-                                            break;
-
-                                        default:
-
-
-
-                                            Console.WriteLine("ingrese una opcion valida");
-
-                                            break;
-
-
-
-
-
-                                    }
-
-                                    
-
-
-
-
-
-
-
-
-
-                                            break;
-
-                                    default:
-
-                                       Console.WriteLine("Ingrese una opcion valida");
-
-                                    break;
-
-                            }
-
-
-                        } while(opcion4 != 0);
-
-                        break;
-
-
-
-                    default:
-
-                        Console.WriteLine("Ingrese una opcion valida");
-
-                        break;
-
-
-
-                } // switch opcion1
-
-
-
-            } while (opcion1 != 0);
-
-
-
-
-
-
-
-        }
-
-    }
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
+    Nombre = "Samuel",
+    Apellido = "Default",
+    Email = "samuel",
+    Telefono = "0000000000"
+}).PasswordHash = "123";
+
+// Seed products (same catalog as the original)
+productRepo.Seed(new List<Product>
+{
+    new Product { Id = 1, Name = "Portatil",        Price = 1000m, StockQuantity = 1 },
+    new Product { Id = 2, Name = "Celular",         Price = 550m,  StockQuantity = 1 },
+    new Product { Id = 3, Name = "Airpods",          Price = 80m,   StockQuantity = 1 },
+    new Product { Id = 4, Name = "Tarjeta Grafica", Price = 800m,  StockQuantity = 1 },
+    new Product { Id = 5, Name = "Mouse",            Price = 22.9m, StockQuantity = 1 },
+    new Product { Id = 6, Name = "Parlantes",        Price = 300m,  StockQuantity = 1 },
+    new Product { Id = 7, Name = "Bose",             Price = 400m,  StockQuantity = 1 },
+});
+
+// Services
+IAuthenticationService authService = new AuthenticationService(userRepo);
+ICart cart = new ShoppingCart();
+IInvoiceService invoiceService = new InvoiceService();
+
+// Payment strategies (OCP: add new methods here without modifying existing code)
+var paymentService = new PaymentService(new IPaymentMethod[]
+{
+    new CardPayment(),
+    new CashPayment(),
+    new AccountPayment()
+});
+
+// Validators (ISP: each validator has a single focused contract)
+IValidator<string> nameValidator = new NameValidator();
+IValidator<string> emailValidator = new EmailValidator();
+IValidator<string> phoneValidator = new PhoneValidator();
+
+// --- Launch the application ---
+var ui = new ConsoleUI(
+    authService,
+    userRepo,
+    productRepo,
+    cart,
+    paymentService,
+    invoiceService,
+    nameValidator,
+    emailValidator,
+    phoneValidator
+);
+
+ui.Run();
